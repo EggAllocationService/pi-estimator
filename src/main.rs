2195676e4cb959216d64bb1e3437983d7cd9a2a6
Kyle Smith <kyleafmine@gmail.com>
2024-04-05 02:56:20 -0300

    Convert to atomic integers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::atomic::{AtomicU64, Ordering};
+
 use rand::{rngs::OsRng, Rng};
 
 
@@ -6,8 +8,8 @@ use rand::{rngs::OsRng, Rng};
 fn main() {
     let mut rng = OsRng;
 
-    let mut in_circle: f64 = 0.0;
-    let mut out_circle: f64 = 0.0;
+    let mut in_circle = AtomicU64::new(0);
+    let mut out_circle = AtomicU64::new(0);
 
     let iters: u64 = std::env::args().nth(1).unwrap().parse().unwrap();
     for _ in 0..iters {
@@ -15,12 +17,15 @@ fn main() {
         let y = rng.gen_range(-1f64..1f64);
 
         if x * x + y * y <= 1.0 {
-            in_circle += 1.0;
+            in_circle.fetch_add(1, Ordering::Relaxed);
         } else {
-            out_circle += 1.0;
+            out_circle.fetch_add(1, Ordering::Relaxed);
         }
     }
 
-    let pi = 4.0 * in_circle / (in_circle + out_circle);
+    let actual_inside = *in_circle.get_mut() as f64;
+    let actual_outside = *out_circle.get_mut() as f64;
+
+    let pi = 4.0 * actual_inside / (actual_inside + actual_outside);
     println!("π ≈ {}", pi);
 }
