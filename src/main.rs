9999494956d6935411c4a44bea01cf7cb22c7734
Kyle Smith <kyleafmine@gmail.com>
2024-04-05 03:04:11 -0300

    Make the iterative method multithreaded

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::sync::atomic::{AtomicU64, Ordering};
+use std::{sync::{atomic::{AtomicU64, Ordering}, Arc}, thread};
 
+use num_cpus::get;
 use rand::{rngs::OsRng, Rng};
 
 
@@ -8,23 +9,38 @@ use rand::{rngs::OsRng, Rng};
 fn main() {
     let mut rng = OsRng;
 
-    let mut in_circle = AtomicU64::new(0);
-    let mut out_circle = AtomicU64::new(0);
+    let in_circle = Arc::new(AtomicU64::new(0));
+    let out_circle = Arc::new(AtomicU64::new(0));
 
     let iters: u64 = std::env::args().nth(1).unwrap().parse().unwrap();
-    for _ in 0..iters {
-        let x = rng.gen_range(-1f64..1f64);
-        let y = rng.gen_range(-1f64..1f64);
 
-        if x * x + y * y <= 1.0 {
-            in_circle.fetch_add(1, Ordering::Relaxed);
-        } else {
-            out_circle.fetch_add(1, Ordering::Relaxed);
-        }
+    let iters_per_thread = iters / num_cpus::get() as u64;
+    let mut handles = Vec::new();
+    for _ in 0..num_cpus::get() {
+        let i = in_circle.clone();
+        let o = out_circle.clone();
+        let handle = thread::spawn(move || {
+            for _ in 0..iters_per_thread {
+                let x = rng.gen_range(-1f64..1f64);
+                let y = rng.gen_range(-1f64..1f64);
+
+                if x * x + y * y <= 1.0 {
+                    i.fetch_add(1, Ordering::Relaxed);
+                } else {
+                    o.fetch_add(1, Ordering::Relaxed);
+                }
+            }
+        });
+        handles.push(handle);
     }
 
-    let actual_inside = *in_circle.get_mut() as f64;
-    let actual_outside = *out_circle.get_mut() as f64;
+    for handle in handles {
+        // wait
+        handle.join().unwrap();
+    }
+
+    let actual_inside = in_circle.fetch_or(0, Ordering::Release) as f64;
+    let actual_outside = out_circle.fetch_or(0, Ordering::Release) as f64;
 
     let pi = 4.0 * actual_inside / (actual_inside + actual_outside);
     println!("π ≈ {}", pi);
