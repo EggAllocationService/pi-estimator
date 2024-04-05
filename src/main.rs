2f5b9f68d1673669f497389e377e5e0838967d51
Kyle Smith <kyleafmine@gmail.com>
2024-04-05 03:07:40 -0300

    Move away from atomic ints for performance?

--- src/main.rs
+++ src/main.rs
@@ -9,38 +9,35 @@ use rand::{rngs::OsRng, Rng};
 fn main() {
     let mut rng = OsRng;
 
-    let in_circle = Arc::new(AtomicU64::new(0));
-    let out_circle = Arc::new(AtomicU64::new(0));
-
     let iters: u64 = std::env::args().nth(1).unwrap().parse().unwrap();
 
     let iters_per_thread = iters / num_cpus::get() as u64;
     let mut handles = Vec::new();
     for _ in 0..num_cpus::get() {
-        let i = in_circle.clone();
-        let o = out_circle.clone();
         let handle = thread::spawn(move || {
+            let mut num_in: u64  = 0;
+            let mut num_out: u64 = 0;
             for _ in 0..iters_per_thread {
                 let x = rng.gen_range(-1f64..1f64);
                 let y = rng.gen_range(-1f64..1f64);
 
                 if x * x + y * y <= 1.0 {
-                    i.fetch_add(1, Ordering::Relaxed);
+                    num_in += 1;
                 } else {
-                    o.fetch_add(1, Ordering::Relaxed);
+                    num_out += 1;
                 }
             }
+            (num_in, num_out) // return result from thread
         });
         handles.push(handle);
     }
 
-    for handle in handles {
-        // wait
-        handle.join().unwrap();
-    }
+    let res = handles.into_iter()
+        .map(|h| h.join().unwrap())
+        .fold((0, 0), |(a, b), (c, d)| (a + c, b + d));
 
-    let actual_inside = in_circle.fetch_or(0, Ordering::Release) as f64;
-    let actual_outside = out_circle.fetch_or(0, Ordering::Release) as f64;
+    let actual_inside = res.0 as f64;
+    let actual_outside = res.1 as f64;
 
     let pi = 4.0 * actual_inside / (actual_inside + actual_outside);
     println!("π ≈ {}", pi);
